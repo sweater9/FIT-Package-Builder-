86f6f133912518b85b33bec26c7a9b96329b072b
sweater9 <sweater.9arc@icloud.com>
2026-07-31 10:40:52 +0400

    Sync deployed builder: next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  poweredByHeader: false,
+  /* config options here */
 };
 
 export default nextConfig;
